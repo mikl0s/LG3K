81f765fe2ab7fb96f7e93bb2305ef32ac9a7f861
mikl0s <mikkel@georgsen.dk>
2024-12-21 22:22:46 +0000

    chore: bump version to 0.5.0

--- lg3k/__init__.py
+++ lg3k/__init__.py
@@ -2,5 +2,5 @@
 
 from .main import generate_log, load_modules
 
-__version__ = "0.4.1"
+__version__ = "0.5.0"
 __all__ = ["generate_log", "load_modules"]
--- setup.py
+++ setup.py
@@ -1,26 +1,37 @@
-"""Setup script for the Log Generator 3000 package."""
+"""Setup script for LG3K."""
 
 from setuptools import find_packages, setup
 
 setup(
     name="lg3k",
-    version="0.4.1",
+    version="0.5.0",
     packages=find_packages(),
     install_requires=[
-        "rich==13.7.0",
+        "click>=8.1.7",
+        "pyyaml>=6.0.1",
+        "rich>=13.7.0",
     ],
-    python_requires=">=3.12",
+    entry_points={
+        "console_scripts": [
+            "lg3k=lg3k.main:main",
+        ],
+    },
     author="mikl0s",
-    author_email="mikkel@dataloes.dk",
+    author_email="mikl0s@example.com",
     description="A versatile log generation tool",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/mikl0s/LG3K",
+    url="https://github.com/mikl0s/lg3k",
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
     ],
+    python_requires=">=3.8",
 )
