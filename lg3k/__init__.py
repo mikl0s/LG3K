7150701e108f9b87a548b3153b6aa9895c5ddeff
mikl0s <mikkel@georgsen.dk>
2024-12-22 02:18:42 +0000

    chore: Bump version to 0.6.0 - Add smart home modules - Add Docker-style progress display - Add synchronized progress updates - Add support for up to 1M log entries per module

--- lg3k/__init__.py
+++ lg3k/__init__.py
@@ -1,6 +1,7 @@
-"""Log Generator 3000 - A versatile log generation tool."""
+"""Log Generator 3000 - A modular log generation tool."""
 
 from .main import load_modules
 
-__version__ = "0.5.0"
+__version__ = "0.6.0"
+__author__ = "Mikkel Munch Mortensen"
 __all__ = ["load_modules"]
